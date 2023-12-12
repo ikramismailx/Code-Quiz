17bc9cfd67494d0216069afb76fe3789312f7157
ikramismailx <ikramismailxx@gmail.com>
2023-12-12 21:54:37 +0000

    added results for options

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ var startScreenEl = document.getElementById("start-screen")
 var questionTitle = document.getElementById("question-title")
 var optionsList = document.querySelectorAll(".options")
 var qno = 0
+var score = 0
 startbutton.addEventListener("click",function(){
   questionsEl.classList.remove("hide") 
   startScreenEl.classList.add("hide") 
@@ -17,4 +18,23 @@ function displayquestion(){
     for(let i=0;i<4;i++){
         optionsList[i].textContent =  questionsList[qno].choices[i]
     }
+}
+
+function checkAnswer(event){
+    var userText = event.target.textContent
+    if (userText == questionsList[qno].answer){
+        score += 5
+    }else{
+
+    }
+    if(qno<questionsList.length-1){
+        qno++
+        displayquestion()
+    }
+}
+
+
+
+for(let i=0;i<4;i++){
+    optionsList[i].addEventListener("click",checkAnswer)
 }
